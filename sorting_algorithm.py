c99a5f3bc513f5c680880c3f5a95e5cf0020f657
Johnny Kessler <68814636+jhKessler@users.noreply.github.com>
2020-09-06 23:52:41 +0200

    Update sorting_algorithm.py

--- sorting_algorithm.py
+++ sorting_algorithm.py
@@ -5,22 +5,34 @@ def create_list(length, parameter):
     random_list = random.choices(digits, k=length)
     return random_list
 
-def sort_list(list):
-    i = 0
+def sort_list(list, reversed=False):
+    i = 0 if not reversed else len(list) - 1
     while True:
         current_item = i
-        next_item = current_item + 1
-        while list[current_item] > list[next_item]:
-            list[current_item], list[next_item] = list[next_item], list[current_item]
-            if current_item > 0:
-                current_item -= 1
-                next_item -= 1
-        if i < len(list) - 2:
-            i += 1
-        else:
-            break
+        next_item = current_item + 1 if not reversed else current_item - 1
+        if not reversed:
+            while list[current_item] > list[next_item]:
+                list[current_item], list[next_item] = list[next_item], list[current_item]
+                if current_item > 0:
+                    current_item -= 1
+                    next_item -= 1
+            if i < len(list) - 2:
+                i += 1
+            else:
+                break  
+                
+        if reversed:
+            while list[current_item] > list[next_item]:
+                list[current_item], list[next_item] = list[next_item], list[current_item]
+                if current_item < len(list) - 1:
+                    current_item += 1
+                    next_item += 1
+            if i > 1:
+                i -= 1
+            else:
+                break
     return list
 
-list = create_list(100, 100)
-print(sort_list(list))
+list = create_list(5, 100)
+print(sort_list(list, True))
 
